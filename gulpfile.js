3f57f9656a82b2c13463fd5a2343bd292cb51cb9
Aaron Bushnell <aaron.bushnell@gmail.com>
2014-06-26 14:45:25 -0400

    Add gulpfile for Stylus compiling

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    stylus = require('gulp-stylus'),
+    prefix = require('gulp-autoprefixer');
+
+var paths = {
+    styles: 'css/**/*.styl',
+    html: './*.html'
+};
+
+gulp.task('connect', function() {
+    connect.server({
+        livereload: true
+    });
+});
+
+gulp.task('html', function () {
+    gulp.src(paths.html)
+        .pipe(connect.reload());
+});
+
+gulp.task('styles', function () {
+    gulp.src(paths.styles)
+        .pipe(stylus())
+        .pipe(prefix())
+        .pipe(gulp.dest('./css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.styles, ['styles']);
+    gulp.watch(paths.html, ['html']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['styles']);
