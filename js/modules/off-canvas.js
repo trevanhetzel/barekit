7f18261de3bda569aa5da84372b84bc05c723088
Trevan Hetzel <trevan@hetzelcreative.com>
2014-08-03 10:31:39 -0500

    Off-Canvas reorganization

--- js/modules/off-canvas.js
+++ js/modules/off-canvas.js
@@ -1,55 +1,81 @@
-jQuery(function ($) {
+(function (Bk, $) {
 
-	$.fn.bnOffCanvas = function (options) {
+	var OffCanvas = function () {
+		this.init.apply(this, arguments);
+	};
 
-		var $offCanvasContain = $(this),
-			$document = $(document),
-			trigger = '.off-canvas-trigger',
-			offCanvasContent = '.off-canvas',
-			openClass = 'off-canvas--open';
+	OffCanvas.defaults = {
+		className: 'off-canvas-contain',
+		trigger: '.off-canvas-trigger',
+		offCanvasContent: '.off-canvas',
+		openClass: 'off-canvas--open'
+	};
 
-		var offCanvas = function () {
-			this.init();
-		};
+	OffCanvas.prototype.init = function (el, options) {
+		var self = this;
+		
+		// Store a reference to the jQuery element
+		this.$el = $(el);
 
-		offCanvas.prototype.init = function () {
+		// Add the class
+		this.$el.addClass(OffCanvas.defaults.className);
 
-			var self = this;
+		$(document)
+			.on('click.bk.offcanvas', function () {
+				self.closeOffCanvas();
+			})
+			.on('click.bk.offcanvas', OffCanvas.defaults.trigger, function (e) {
+				var $this = $(this),
+					$offCanvas = $this.parents('.off-canvas-contain');
 
-			$document
-				.on('click', function () {
+				e.stopPropagation();
+				e.preventDefault();
+
+				if ($offCanvas.hasClass(OffCanvas.defaults.openClass)) {
 					self.closeOffCanvas();
-				})
-				.on('click', trigger, function (e) {
-					var $this = $(this),
-						$offCanvas = $this.parents('.off-canvas-contain');
+				} else {
+					self.openOffCanvas($this, $offCanvas);
+				}
+			})
+			.on('click.bk.offcanvas', OffCanvas.defaults.offCanvasContent, function (e) {
+				e.stopPropagation();
+			});
 
-					e.stopPropagation();
-					e.preventDefault();
-
-					if ($offCanvas.hasClass(openClass)) {
-						self.closeOffCanvas();
-					} else {
-						self.openOffCanvas($this, $offCanvas);
-					}
-				})
-				.on('click', offCanvasContent, function (e) {
-					e.stopPropagation();
-				});
+		OffCanvas.prototype.closeOffCanvas = function () {
+			this.$el.removeClass(OffCanvas.defaults.openClass);
 		};
 
-		offCanvas.prototype.closeOffCanvas = function () {
-			$offCanvasContain.removeClass(openClass);
+		OffCanvas.prototype.openOffCanvas = function ($this, $offCanvas) {
+			$offCanvas.addClass(OffCanvas.defaults.openClass);
 		};
-
-		offCanvas.prototype.openOffCanvas = function ($this, $offCanvas) {
-			$offCanvas.addClass(openClass);
-		};
-
-		$offCanvasContain.length ? new offCanvas : false;
 
 	};
 
-	$('.off-canvas-contain').bnOffCanvas();
+	OffCanvas.prototype.destroy = function () {
+		this.$el.off(OffCanvas.defaults.className);
+	};
 
-});
+	Bk.OffCanvas = OffCanvas;
+
+	// Expose as a jQuery Plugin
+	$.fn.bnOffCanvas = function (options) {
+		return this.each(function () {
+			var $el = $(this);
+			// Check if it is already set up
+			if (!$el.data('bnOffCanvas')) {
+				$el.data('bnOffCanvas', new OffCanvas(this, options));
+			}
+		});
+	};
+
+	// Allow defaults to be accessed via a common jQuery pattern
+	$.fn.bnOffCanvas.defaults = OffCanvas.defaults;
+
+	// Auto-initialize if set
+	jQuery(function ($) {
+		if (Bk.autoInitialize) {
+			$('.' + OffCanvas.defaults.className).bnOffCanvas();
+		}
+	});
+
+}( Barekit, jQuery ));
